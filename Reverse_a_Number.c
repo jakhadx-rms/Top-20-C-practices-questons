76524923dd563c1025385686658d3c08466370ec
Ajay Jakhad <jakhadx@gmail.com>
2025-11-03 17:12:07 +0530

    Update Reverse_a_Number.c

--- Reverse_a_Number.c
+++ Reverse_a_Number.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
 
 int main() {
-   
+   int n=570;
+	int rim,rev=0;
+	while(n>0){
+		rim=n%10;
+		rev=rev*10+rim;
+		n=n/10;
+	}
+    printf("revers the Number : %d", rev);
+	return 0;
     return 0;
 }
