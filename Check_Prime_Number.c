e12eb3adf0b8098ac45d8aa8711b275420130b9e
Ajay Jakhad <jakhadx@gmail.com>
2025-11-03 15:30:43 +0530

    Update Check_Prime_Number.c

--- Check_Prime_Number.c
+++ Check_Prime_Number.c
@@ -1,6 +1,27 @@
 #include<stdio.h>
    
 int main() {
+   int num;
+  printf("Enter a number of check Prime or not : ");
+  scanf("%d",&num);
    
+  int i=2;
+  int k=0;
+  if (num<=1){
+  	  	 printf("Yes the number is not a prime number");
+		}
+  while(i<=num/2){
+  	  if (num%i==0){
+  	  	 k++;
+		}
+	  i++;
+  }  
+  if (k==0){
+  	  	 printf("Yes the number is a prime number");
+		}
+		else{
+			printf("Number is not a prime number");
+		}
+  
     return 0;
 }
