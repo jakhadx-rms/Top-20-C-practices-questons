d7189c8b728add5d9ae9b135b9423731c3d484cd
Ajay Jakhad <jakhadx@gmail.com>
2025-11-03 12:31:07 +0530

    Update Count_Digits.c

--- Count_Digits.c
+++ Count_Digits.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
 
 int main() {
-   
+   int n=151;
+   int count=0;
+   while(n>0){
+       n/=10;
+      count++;
+   }
+   printf("Number of digits : %d",count);
     return 0;
 }
