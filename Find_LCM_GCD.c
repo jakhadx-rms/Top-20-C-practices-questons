e8b5f3880d58aa76827786a83eb5375f1b576ed8
Ajay Jakhad <jakhadx@gmail.com>
2025-11-03 17:41:20 +0530

    Update Find_LCM_GCD.c

--- Find_LCM_GCD.c
+++ Find_LCM_GCD.c
@@ -1,6 +1,23 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main() {
-   
+    int a, b;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    int x = a, y = b;
+
+   while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+
+    int gcd = a;
+    int lcm = (x * y) / gcd;
+
+    printf("GCD of %d and %d = %d\n", x, y, gcd);
+    printf("LCM of %d and %d = %d\n", x, y, lcm);
+
     return 0;
 }
