cf6a5194948f91075b606adb23b0239cd80d3736
Ajay Jakhad <jakhadx@gmail.com>
2025-11-03 15:54:13 +0530

    Implement Fibonacci series calculation
    Initialize Fibonacci series with first two terms and print the series up to the 10th term.

--- Fibonacci_series.c
+++ Fibonacci_series.c
@@ -1,6 +1,16 @@
 #include<stdio.h>
 
 int main() {
-   
+  int t1=0;
+  int t2=1;
+  printf("Fibonacci series : ");
+  printf("%d %d ",t1, t2);
+  int temp;
+  for(int i=3 ; i<=10 ; i++){
+  	temp=t1+t2;
+  	printf("%d ",temp);
+  	t1=t2;
+  	t2=temp; 
+  }
     return 0;
 }
