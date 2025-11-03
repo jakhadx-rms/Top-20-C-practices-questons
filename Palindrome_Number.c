519f61c7fcc5964814d594b34316e13e2141a015
Ajay Jakhad <jakhadx@gmail.com>
2025-11-03 17:31:50 +0530

    Update Palindrome_Number.c

--- Palindrome_Number.c
+++ Palindrome_Number.c
@@ -1,6 +1,21 @@
-#include<stdio.h>
-   
+#include <stdio.h>
+
 int main() {
-   
+    int n = 570;
+    int temp = n;
+    int rim, rev = 0;
+
+    while (n > 0) {
+        rim = n % 10;
+        rev = rev * 10 + rim;
+        n = n / 10;
+    }
+
+    if (temp == rev) {
+        printf("Palindrome Number: %d\n", rev);
+    } else {
+        printf("Not a Palindrome Number\n");
+    }
+
     return 0;
 }
