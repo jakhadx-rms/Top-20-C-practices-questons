c20f58d5a1237b9f3b4a10ff3caaa47617f0a72c
Ajay Jakhad <jakhadx@gmail.com>
2025-11-03 17:11:51 +0530

    Calculate the sum of digits of a number

--- Sum_of_Digits.c
+++ Sum_of_Digits.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
 
 int main() {
-   
+   int n=570;
+	int rim,sum=0;
+	while(n>0){
+		rim=n%10;
+		sum=sum+rim;
+		n=n/10;
+		
+	}
+   	printf("Sum of digits : %d \n",sum);
     return 0;
 }
