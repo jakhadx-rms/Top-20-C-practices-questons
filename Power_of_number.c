cdc2de15b699e3dec55da153f8959ace2c5e9b48
Ajay Jakhad <jakhadx@gmail.com>
2025-11-03 17:38:03 +0530

    Implement power calculation in Power_of_number.c
    Added functionality to calculate power of a number.

--- Power_of_number.c
+++ Power_of_number.c
@@ -1,6 +1,18 @@
 #include<stdio.h>
-
 int main() {
-   
+    int base, exp;
+    long long pow = 1;   // use long long for large powers  otherwise int
+
+    printf("Enter base: ");
+    scanf("%d", &base);
+    printf("Enter exponent: ");
+    scanf("%d", &exp);
+
+    for (int i = 1; i <= exp; i++) {
+        pow *= base;
+    }
+
+    printf("%lld\n",pow);
+
     return 0;
 }
