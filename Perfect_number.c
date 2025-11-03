ddfc5eb88aca01b060d3f553d2f3653dabaecd59
Ajay Jakhad <jakhadx@gmail.com>
2025-11-03 17:33:18 +0530

    Fix formatting and improve comments in Perfect_number.c

--- Perfect_number.c
+++ Perfect_number.c
@@ -1,6 +1,18 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main() {
-   
+    int n = 28;  // try with 6, 28, 496...
+    int sum = 0;
+
+    for (int i = 1; i <= n / 2; i++) {
+        if (n % i == 0)
+            sum += i;
+    }
+
+    if (sum == n)
+        printf("%d is a Perfect Number\n", n);
+    else
+        printf("%d is NOT a Perfect Number\n", n);
+
     return 0;
 }
