2f3c94bfce6a52f50d0431752dd458dcfe1a5816
Ajay Jakhad <jakhadx@gmail.com>
2025-11-03 17:16:35 +0530

    Update Strong_number.c

--- Strong_number.c
+++ Strong_number.c
@@ -1,6 +1,28 @@
-#include<stdio.h>
+#include <stdio.h>
+
+int fact(int a) {
+    int facto = 1;
+    for (int i = 2; i <= a; ++i){
+    	facto*=i;
+	}
+    return facto;
+}
 
 int main() {
-   
+    int n = 125;
+    int temp = n;
+    int sum = 0;
+
+    while (n > 0) {
+        int digit = n % 10;
+        sum += fact(digit);
+        n /= 10;
+    }
+
+    if (sum == temp) {
+        printf("%d is a Strong number\n", temp);
+    } else {
+        printf(" NOT a Strong number\n");
+    }
     return 0;
 }
