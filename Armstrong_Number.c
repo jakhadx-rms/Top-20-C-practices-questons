ddbe6a17d25dd2f63b5e589180d389b09338a748
Ajay Jakhad <jakhadx@gmail.com>
2025-11-03 17:00:44 +0530

    Update Armstrong_Number.c

--- Armstrong_Number.c
+++ Armstrong_Number.c
@@ -1,6 +1,34 @@
 #include<stdio.h>
    
 int main() {
+     int n = 153;        
+    int temp = n;
+    int a = 0, sum = 0;
+
    
+    while (temp > 0) {
+        a++;
+        temp /= 10;
+    }
+
+    temp = n; 
+
+   
+    while (temp > 0) {
+        int digit = temp % 10;
+        int power = 1;
+        for (int i = 1; i <= a; i++) {
+            power *= digit;
+        }
+
+        sum += power;
+        temp /= 10;
+    }
+
+    if (sum == n)
+        printf("%d is an Armstrong number\n", n);
+    else
+        printf("%d is NOT an Armstrong number\n", n);
+
     return 0;
 }
